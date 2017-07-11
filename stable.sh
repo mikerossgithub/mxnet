c5d6e64585735f9e20ceba2cf3201fcd54661380
Jameson Merkow <jmerkow@gmail.com>
2017-07-11 07:47:56 -0700

    creation script

---
+++ stable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git checkout master
+git submodule init
+git submodule sync
+git submodule update
+git branch -D stable
+git checkout -b stable
+## merge PRs
+#hub merge ....
+## commit
+git add stable.sh
+git commit -m 'creation script'
